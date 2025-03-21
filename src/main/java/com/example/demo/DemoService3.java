3b578d7af243f3b8e4ce54e74d9f777b3a987f9f
zhangld <cherrycheek@163.com>
2025-03-21 17:12:08 +0800

    my commit

--- src/main/java/com/example/demo/DemoService3.java
+++ src/main/java/com/example/demo/DemoService3.java
@@ -2,7 +2,8 @@ package com.example.demo;
 
 public class DemoService3
 {
-    public void modifyPraram3(String name){
+    public void modifyPraram4(String name){
         MyUtil.modify(name);
+        System.out.println("hello");
     }
 }
