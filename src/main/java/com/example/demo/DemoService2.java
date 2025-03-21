ed56f005ffd049a83749006d494e93a89b066446
zhangld <cherrycheek@163.com>
2025-03-21 16:46:04 +0800

    my demo

--- src/main/java/com/example/demo/DemoService2.java
+++ src/main/java/com/example/demo/DemoService2.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class DemoService2
 {
-    public void modifyPraram(String name){
+    public void modifyPraram2(String name){
         MyUtil.modify(name);
     }
 }
